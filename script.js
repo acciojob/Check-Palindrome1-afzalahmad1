e21317759eeff132657dee740171b934eca17058
Afzal Ahmad <113641536+afzalahmad1@users.noreply.github.com>
2023-04-05 16:26:35 +0530

    created a submission

--- script.js
+++ script.js
@@ -3,13 +3,13 @@
 let low = 0;
 
     // loop through half of the string
-while(high<low){
-	if(string(high)!==string(low)){
-		return false;
-	}
-	high++;
-	low--;
-}
+    for (let i = 0; i < len / 2; i++) {
+
+        // check if first and last string are same
+        if (string[i] !== string[len - 1 - i]) {
+            return false;
+        }
+    }
     return true;
 }
 module.exports = palindrome
